ef133b8bd8cc1551a1767b835a20e5814afc6679
Екатерина Шмелева <rinashmeleva@MacBook-Air-Ekaterina.local>
2019-01-29 18:48:34 +0200

    add bootstrap and static files

--- game_store/settings.py
+++ game_store/settings.py
@@ -122,9 +122,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Static files
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATICFILES_DIRS = (
+    '/Users/rinashmeleva/wsd/static',
+)
 
 STATIC_URL = '/static/'
 
