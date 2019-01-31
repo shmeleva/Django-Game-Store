8f2088de2f4fd2f40df67253b1534a4332e5962a
Gonzalez Carceller Felipe <gonzalf1@niksula.hut.fi>
2019-01-31 19:15:50 +0200

    display all games and their pages

--- game_store/apps/games/views.py
+++ game_store/apps/games/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from django.http import Http404
+from .models import Game
 
 def games(req):
-    return render(req, 'games.html')
+    all_games = Game.objects.all()
+    return render(req, 'games.html', { 'all_games': all_games })
 
 def game(req, id):
-    return render(req, 'game.html')
+    try:
+        game = Game.objects.get(pk = id)
+    except Game.DoesNotExist:
+        raise Http404('Game does not exist')
+    return render(req, 'game.html', { 'game': game })
 
 def play(req, id):
     return render(req, 'play.html')
