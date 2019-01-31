c83eddcd85405f7f22dbed850b073609ca65fff6
Gonzalez Carceller Felipe <gonzalf1@niksula.hut.fi>
2019-01-31 23:43:25 +0200

    add leaderboards page

--- game_store/apps/results/views.py
+++ game_store/apps/results/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def leaderboards(req):
+    return render(req, 'leaderboards.html')
--- game_store/urls.py
+++ game_store/urls.py
@@ -18,16 +18,18 @@ from django.urls import path
 from game_store import views
 from game_store.apps.users import views as users_views
 from game_store.apps.games import views as games_views
+from game_store.apps.results import views as results_views
 from game_store.apps.purchases import views as purchases_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', games_views.games), # Unauthorized users can and players
+    path('', games_views.games, name='default_view'), # Unauthorized users can and players
     # can search games, players can also see their games, developers
     # can only search their own games and add new games.
     path('login/', users_views.login, name='login'), # Only for unauthorized users.
     path('logout/', users_views.logout, name='logout'),
     path('register/', users_views.register, name='register'),
+    path('leaderboards', results_views.leaderboards, name='leaderboards'),
     path('game/<int:id>', games_views.game, name='game_page'), # Unauthorized users can sign in,
     # players can buy and play a game, developers can edit a game
     path('game/<int:id>/purchase', purchases_views.purchase), # Only for players.
