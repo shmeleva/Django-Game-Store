7afd2d17f8e2c7270913fa3eefc72507ab521cfc
Gonzalez Carceller Felipe <gonzalf1@niksula.hut.fi>
2019-02-01 02:08:12 +0200

    add str to Purchases

--- game_store/apps/purchases/models.py
+++ game_store/apps/purchases/models.py
@@ -6,3 +6,6 @@ class Purchase(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return self.user.user.username + " - " + self.game.title
