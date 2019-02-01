c697af608da2a952634b4d08884e26969512faf1
Gonzalez Carceller Felipe <gonzalf1@niksula.hut.fi>
2019-02-01 02:08:30 +0200

    add owned games page

--- game_store/apps/games/views.py
+++ game_store/apps/games/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Game
+from game_store.apps.purchases.models import Purchase
+from game_store.apps.users.models import UserProfile
 
-def games(req):
-    all_games = Game.objects.all()
-    return render(req, 'games.html', { 'all_games': all_games })
+def all_games(req):
+    games = Game.objects.all()
+    return render(req, 'games.html', { 'games': games })
+
+# TODO: clean this code
+def owned_games(req):
+    up = get_object_or_404(UserProfile, user=req.user)
+    purchases = Purchase.objects.filter(user=up)
+    games = set()
+    for purchase in purchases:
+        games.add(purchase.game)
+    return render(req, 'games.html', { 'games': games })
 
 def game(req, id):
     game = get_object_or_404(Game, pk=id)
