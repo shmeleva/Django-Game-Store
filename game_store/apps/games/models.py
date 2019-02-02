7ee854701151fe485651657c3c01f0ae424f573c
Ekaterina Shmeleva <ekaterina.shmeleva@aalto.fi>
2019-02-02 18:10:38 +0200

    only allow positive prices

--- game_store/apps/games/models.py
+++ game_store/apps/games/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 from game_store.apps.users.models import UserProfile
 from game_store.apps.categories.models import Category
+from decimal import *
 
 class Game(models.Model):
     title = models.CharField(max_length=128)
     image = models.ImageField(upload_to='images/games')
     description = models.TextField()
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2, validators=[MinValueValidator(Decimal('0.01'))])
     url = models.URLField(max_length=128)
     developer = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     categories = models.ManyToManyField(Category, blank=True)
