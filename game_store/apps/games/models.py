344e670de73858cdbaba9064fb2f846d5d295808
Ekaterina Shmeleva <ekaterina.shmeleva@aalto.fi>
2019-02-03 15:22:25 +0200

    add TODO

--- game_store/apps/games/models.py
+++ game_store/apps/games/models.py
@@ -11,7 +11,7 @@ class Game(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2, validators=[MinValueValidator(Decimal('0.01'))])
     url = models.URLField(max_length=128)
     developer = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    categories = models.ManyToManyField(Category, blank=True)
+    categories = models.ManyToManyField(Category, blank=True) # TODO: Make blank=False
 
     def __str__(self):
         return self.title
