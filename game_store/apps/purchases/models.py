05b6231dccbb8e0e0a8afc388b4da890a194c5c8
Sataponn Phutrakul <sataponn.phutrakul@aalto.fi>
2019-02-06 18:00:39 +0200

    Add price in model Purchase

--- game_store/apps/purchases/models.py
+++ game_store/apps/purchases/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from decimal import *
 from game_store.apps.users.models import UserProfile
 from game_store.apps.games.models import Game
 
 class Purchase(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=8, decimal_places=2, validators=[MinValueValidator(Decimal('0.01'))])
     timestamp = models.DateTimeField(auto_now_add=True, blank=True)
 
     def __str__(self):
