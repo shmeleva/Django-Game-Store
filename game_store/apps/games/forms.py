10267b6382c849ab3d22cbd676da6920307968df
Sataponn Phutrakul <sataponn.phutrakul@aalto.fi>
2019-02-06 12:15:29 +0200

    Fix game-category relationship

--- game_store/apps/games/forms.py
+++ game_store/apps/games/forms.py
@@ -5,18 +5,12 @@ from game_store.apps.games.models import Game
 from game_store.apps.categories.models import Category
 
 class PublishForm(ModelForm):
-    categories = forms.ModelMultipleChoiceField(
-        widget=forms.CheckboxSelectMultiple,
-        queryset=Category.objects.all()
-    )
-
     class Meta:
         model = Game
-        fields = ('title', 'image', 'description', 'price' , 'url')
-
-    def save(self, commit=True):
-        game = super(PublishForm, self).save(commit=True)
-        return game
+        fields = ('title', 'image', 'description', 'price' , 'url', 'categories')
+        widgets = {
+            'categories': forms.CheckboxSelectMultiple(),
+        }
 
 class SearchForm(Form):
     query = forms.CharField(
