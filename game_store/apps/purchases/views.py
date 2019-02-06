2bc81064f8c8364394109ec193bb5d3bd61db307
Sataponn Phutrakul <sataponn.phutrakul@aalto.fi>
2019-02-06 17:31:59 +0200

    WIP Handle payment

--- game_store/apps/purchases/views.py
+++ game_store/apps/purchases/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from game_store.apps.games.models import Game
+from game_store.apps.purchases.models import Purchase
+from game_store.apps.purchases.forms import PurchaseForm
 
+@login_required(login_url='/login/')
 def purchase(req, id):
-    return render(req, 'purchase.html')
+    # TODO: Check if the user has already purchased the game
+
+    game = get_object_or_404(Game, pk=id)
+
+    if req.method == 'POST':
+        form = PurchaseForm(req.POST)
+
+        if form.is_valid():
+            # TODO: Call the API
+    else:
+        form = PurchaseForm()
+
+    return render(req, 'purchase.html', {
+        'form': form,
+        'game': game,
+    })
 
 def stats(req):
     return render(req, 'stats.html')
