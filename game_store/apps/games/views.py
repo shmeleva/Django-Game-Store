d1841d350afdcd473f09600c5e854e3c0d136bae
Ekaterina Shmeleva <ekaterina.shmeleva@aalto.fi>
2019-02-07 13:49:06 +0200

    filter all games before searching

--- game_store/apps/games/views.py
+++ game_store/apps/games/views.py
@@ -17,9 +17,20 @@ from game_store.apps.games.forms import SearchForm
 from game_store.apps.games.utils import SearchBuilder
 
 def all_games(req):
+    user = UserProfile.get_user_profile_or_none(req.user)
+    games = None
+
+    if user is None:
+        games = Game.objects.all()
+    else:
+        if user.is_player:
+            games = SearchBuilder(Game.objects.all(), user).set_ownership_flags().set_highscores().games
+        else:
+            games = Game.objects.filter(developer__exact=user)
+
     return render(req, 'games.html', {
-        'games': Game.objects.all(),
-        'user_profile': UserProfile.get_user_profile_or_none(req.user),
+        'games': games,
+        'user_profile': user,
         'search_form': SearchForm(),
     })
 
@@ -53,7 +64,7 @@ def search(req):
 
             if user.is_developer:
                 return SearchBuilder(
-                    Game.objects.filter(developer__exact=user),user
+                    Game.objects.filter(developer__exact=user), user
                 ).apply_rules(
                     query,categories
                 ).build()
