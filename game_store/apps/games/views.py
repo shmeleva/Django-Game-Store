3e6b331519e21c0de9fff01c8b90d73d7c7ac46b
Ekaterina Shmeleva <ekaterina.shmeleva@aalto.fi>
2019-02-08 12:05:33 +0200

    add comments for game views

--- game_store/apps/games/views.py
+++ game_store/apps/games/views.py
@@ -18,19 +18,26 @@ from game_store.apps.games.forms import SearchForm
 from game_store.apps.games.utils import SearchBuilder
 
 
-# All games:
+# All games.
+# Accepts: GET / requests.
+# Returns: an HttpResponse containing a rendered HTML page.
 def games(req):
 
     user = UserProfile.get_user_profile_or_none(req.user)
-    games = None
 
-    if user is None:
-        games = Game.objects.all()
-    else:
+    if user is not None:
         if user.is_player:
-            games = SearchBuilder(Game.objects.all(), user).set_ownership_flags().set_highscores().games
-        else:
+            # Setting ownership and highscore fields for player-owned games.
+            games = SearchBuilder(Game.objects.all(), user) \
+            .set_ownership_flags() \
+            .set_highscores().games
+        if user.is_developer:
+            # Only showing developers their own games.
             games = Game.objects.filter(developer__exact=user)
+
+    # Showing all games for all other users:
+    if games is None:
+        games = Game.objects.all()
 
     return render(req, 'games.html', {
         'games': games,
@@ -38,7 +45,9 @@ def games(req):
         'search_form': SearchForm(),
     })
 
-# Game search results:
+# Search results.
+# Accepts: POST /search requests.
+# Returns: a JsonResponse containing a rendered HTML partial view.
 def search(req):
 
     form = SearchForm(req.POST)
@@ -54,6 +63,7 @@ def search(req):
 
             if user.is_player:
                 if player_games_only:
+                    # Only showing player-owned games.
                     purchases = Purchase.objects.values('game').filter(user__exact=user)
                     return SearchBuilder(Game.objects.filter(id__in=purchases), user) \
                     .apply_rules(query,categories) \
@@ -61,6 +71,8 @@ def search(req):
                     .set_highscores() \
                     .build()
                 else:
+                    # Searching through all games, while setting correct ownership
+                    # and highscore fields for player-owned games.
                     return SearchBuilder(Game.objects, user) \
                     .apply_rules(query, categories) \
                     .set_ownership_flags() \
@@ -68,23 +80,34 @@ def search(req):
                     .build()
 
             if user.is_developer:
+                # Only searching through games publised by the developer.
                 return SearchBuilder(Game.objects.filter(developer__exact=user), user) \
                 .apply_rules(query,categories) \
                 .build()
 
+        # For all other users, including unauthorized users,
+        # searching through all games.
         return SearchBuilder(Game.objects, user) \
         .apply_rules(query, categories) \
         .build()
     else:
         return JsonResponse({ }) #TODO
 
-# A game:
+# A game.
+# Accepts: GET /game/{id} requests.
+# Returns: an HttpResponse containing a rendered HTML page.
+# Errors:
+# - 401, if game does exist but the user has no access to see the game
+# (e.g. developers can only see and manage their own games). TODO
+# - 404, if Game.DoesNotExist.
 def game(req, id):
 
     user = UserProfile.get_user_profile_or_none(req.user)
     game = get_object_or_404(Game, pk=id)
 
-    highscores = Result.objects.values('user').annotate(highscore=Max('score')).order_by('-highscore')[:10]
+    highscores = Result.objects.values('user') \
+    .annotate(highscore=Max('score')) \
+    .order_by('-highscore')[:10]
     logger.error(highscores)
 
     if Purchase.objects.filter(user=user, game=game).count() > 0:
@@ -100,6 +123,12 @@ def game(req, id):
 def play(req, id):
     return render(req, 'play.html', { 'game': get_object_or_404(Game, pk=id) })
 
+# A game publication form.
+# Accepts: GET /publish and POST /publish requests.
+# Returns: HttpResponse with an HTML page for GET requests,
+# a redirect to /game/{id} for successful POST requests
+# Errors:
+# - Redirects non-developers to /. TODO: return 401 instead
 def publish(req):
 
     user = UserProfile.get_user_profile_or_none(req.user)
@@ -120,3 +149,22 @@ def publish(req):
         'form': form,
         'user_profile': user,
     })
+
+# A game editing form.
+# Accepts: GET /game/{id}/edit and POST /game/{id}/edit requests.
+# Returns: HttpResponse with an HTML page for GET requests,
+# a redirect to /game/{id} for successful POST requests
+# Errors:
+# - Redirects non-developers to /. TODO: return 401 instead
+# - 401, if the developer does not own the game.
+def edit(req):
+    pass
+
+# A game editing form.
+# Accepts: GET /game/{id}/stats.
+# Returns: HttpResponse with an HTML page.
+# Errors:
+# - Redirects non-developers to /. TODO: return 401 instead
+# - 401, if the developer does not own the game.
+def stats(req):
+    pass
