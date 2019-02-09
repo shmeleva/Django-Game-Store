f3a2ff3793e74faeb4ad5aeb07fb9876a0df8cdf
Sataponn Phutrakul <sataponn.phutrakul@aalto.fi>
2019-02-09 12:43:48 +0200

    Require users to put full names

--- game_store/apps/users/forms.py
+++ game_store/apps/users/forms.py
@@ -5,13 +5,15 @@ from game_store.apps.users.models import UserRole, UserProfile
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True, max_length=30)
+    last_name = forms.CharField(required=True, max_length=150)
     role = forms.ChoiceField(
         choices=[(role.value, role.name) for role in UserRole],
     )
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2', 'first_name', 'last_name')
 
     def save(self, commit=True):
         user = super(RegisterForm, self).save(commit=True)
