5dbab880093a5717f2c4c98deb00e526cd7b9283
Sataponn Phutrakul <sataponn.phutrakul@aalto.fi>
2019-02-13 19:46:06 +0200

    Don't allow doing anything if users haven't chosen roles

--- game_store/settings.py
+++ game_store/settings.py
@@ -66,6 +66,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    'game_store.apps.users.middleware.UserRoleValidationMiddleware',
 ]
 
 ROOT_URLCONF = 'game_store.urls'
