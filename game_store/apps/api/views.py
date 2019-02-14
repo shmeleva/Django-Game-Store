49d034e27fde625e8d0c3078563556fa5126e603
Ekaterina Shmeleva <ekaterina.shmeleva@aalto.fi>
2019-02-14 15:02:49 +0200

    add filtering by categories

--- game_store/apps/api/views.py
+++ game_store/apps/api/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from django.db.models import Q
+from functools import reduce
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
 
 from game_store.apps.games.models import Game
+from game_store.apps.categories.models import Category
 
 from .serializers import GamesSerializer
 
@@ -14,15 +17,21 @@ class ListGamesView(generics.ListAPIView):
     def get_queryset(self):
         queryset = Game.objects.all()
 
-        query = self.request.query_params.get('query', None)
-        if query is not None:
-            queryset = queryset.filter(title__contains=query)
+        search_query_param = self.request.query_params.get('search', None)
+        if search_query_param:
+            queryset = queryset.filter(title__contains=search_query_param)
 
-        category_names = self.request.query_params.get('categories', None)
-        print(category_names)
-        #if category_names is not None:
-
-
+        categories_query_param = self.request.query_params.get('categories', None)
+        if categories_query_param:
+            categories_as_strings = categories_query_param.split(",")
+            q_list = map(lambda n: Q(title__iexact=n), categories_as_strings)
+            q_list = reduce(lambda a, b: a | b, q_list)
+            categories =  Category.objects.filter(q_list)
+            if categories.exists():
+                queryset = queryset.filter(categories__in=categories).distinct()
+            else:
+                queryset = Game.objects.none()
+                
         return queryset
 
 #class HelloView(APIView):
