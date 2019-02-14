e5a28c164cce810f8561486257ca896d3debbbfc
Sataponn Phutrakul <sataponn.phutrakul@aalto.fi>
2019-02-14 20:16:50 +0200

    Mark purchase as canceled when canceling from credit card page

--- game_store/urls.py
+++ game_store/urls.py
@@ -42,6 +42,7 @@ urlpatterns = [
     path('game/<int:id>/edit', games_views.edit, name='edit'), # Only for developers.
     path('game/<int:id>/delete', games_views.delete, name='delete'), # Only for developers.
     path('game/<int:id>/purchase', purchases_views.purchase, name='purchase'), # Only for players.
+    path('game/<int:id>/purchase/<pid>/cancel', purchases_views.cancel_purchase, name='cancel_purchase'),
     path('game/<int:id>/play', games_views.play, name='play'), # Only for players.
     path('game/update_score', games_views.update_score, name='update_score'),
     path('game/save_game', games_views.save_game, name='save_game'),
